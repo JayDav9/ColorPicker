ffb366238a4e4b8c99928eae15bca4b29996aa2e
JD <jaydav95@outlook.com>
2022-11-18 12:03:45 -0600

    intial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
+  function getRandomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+  }
+
+  function changeGameColor() {
+    let newColor = getRandomColor();
+    setCurrentColor(newColor);
+    getNewColors(newColor)
+
+  
+  }
+
+  function getNewColors(correctColor) {
+    let newColors = [correctColor, getRandomColor(), getRandomColor()];
+    setButtonColors(newColors);
+
+  }
+
+  const [currentColor, setCurrentColor] = useState(getRandomColor());
+  let initialButtonColors = [currentColor, getRandomColor(), getRandomColor()];
+
+  const [buttonColors, setButtonColors] = useState(initialButtonColors);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ backgroundColor: currentColor, height: "500px" }}> </div>
+
+      {buttonColors.map((color) => {
+        return (
+          <button
+            key={color}
+            onClick={color === currentColor ? changeGameColor : null}
+          >
+            {color}
+          </button>
+        );
+      })}
+
+<br></br>
+      <button onClick={changeGameColor}>{buttonColors[0]}</button>
     </div>
   );
 }
